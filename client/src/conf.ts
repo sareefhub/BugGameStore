a70e4999548b3ea46540f6aedb7784bc0fe6cafa
ซารีฟ มะสาแม็ง <sareef.masamaeng@gmail.com>
2024-03-05 02:48:52 +0700

    fix url production

--- client/src/conf.ts
+++ client/src/conf.ts
@@ -2,7 +2,7 @@ const isProd = process.env.NODE_ENV === 'production';
 
 const conf = {
     isProd,
-    apiPrefix: isProd? 'http://localhost:1337' : 'http://localhost:1337'
+    apiPrefix: isProd? 'https://sda-project-416115.as.r.appspot.com' : 'https://sda-project-416115.as.r.appspot.com'
 }
 
 export default conf;
